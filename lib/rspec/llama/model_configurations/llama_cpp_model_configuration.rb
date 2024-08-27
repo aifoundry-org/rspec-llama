07602d7642e3abffd509d118a11e4bb59cd57588
White Rabbit <wwwwhiterabbit@proton.me>
2024-08-27 20:36:37 +0400

    llama_cpp runner and model configuration

--- lib/rspec/llama/model_configurations/llama_cpp_model_configuration.rb
+++ lib/rspec/llama/model_configurations/llama_cpp_model_configuration.rb
@@ -4,15 +4,44 @@ module RSpec
   module Llama
     class LlamaCppModelConfiguration
       DEFAULT_TEMPERATURE = 0.5
-      DEFAULT_N_PREDICT = 500
+      DEFAULT_THREADS = 4
+      DEFAULT_N_PREDICT = -1 # Generate until context is filled
       DEFAULT_STOP = /\A<(?:end|user|assistant|endoftext|system)>\z/
 
-      attr_reader :temperature, :n_predict, :stop
+      attr_reader :model_path, :temperature, :threads, :n_predict, :additional_options
 
-      def initialize(temperature: DEFAULT_TEMPERATURE, n_predict: DEFAULT_N_PREDICT, stop: DEFAULT_STOP)
+      def initialize(
+        model_path:,
+        temperature: DEFAULT_TEMPERATURE,
+        threads: DEFAULT_THREADS,
+        n_predict: DEFAULT_N_PREDICT,
+        **additional_options
+      )
+        @model_path = model_path
         @temperature = temperature
+        @threads = threads
         @n_predict = n_predict
-        @stop = stop
+        @additional_options = additional_options
+      end
+
+      def to_a
+        cli_options = [
+          '--model', @model_path,
+          '--temp', @temperature.to_s,
+          '--threads', @threads.to_s,
+          '--predict', @n_predict.to_s
+        ]
+
+        # Add additional options in key-value pair format
+        @additional_options.each do |option, value|
+          if value == true
+            cli_options << "--#{option}" # For flags
+          else
+            cli_options << "--#{option}" << value.to_s # For key-value pairs
+          end
+        end
+
+        cli_options
       end
     end
   end
--- lib/rspec/llama/model_runner_results/llama_cpp_model_runner_result.rb
+++ lib/rspec/llama/model_runner_results/llama_cpp_model_runner_result.rb
@@ -3,6 +3,15 @@
 module RSpec
   module Llama
     class LlamaCppModelRunnerResult
+      attr_reader :result
+
+      def initialize(result)
+        @result = result
+      end
+
+      def to_s
+        result.strip # Removes any leading/trailing whitespace or newlines
+      end
     end
   end
 end
