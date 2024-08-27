f3e53ac10c2a216b60ed88a22ad02a3755c29c53
White Rabbit <wwwwhiterabbit@proton.me>
2024-08-27 22:48:30 +0400

    additional options for openai configuration

--- lib/rspec/llama/model_configurations/openai_model_configuration.rb
+++ lib/rspec/llama/model_configurations/openai_model_configuration.rb
@@ -6,15 +6,16 @@ module RSpec
       DEFAULT_MODEL = 'gpt-3.5-turbo'
       DEFAULT_TEMPERATURE = 0.5
 
-      attr_reader :model, :temperature
+      attr_reader :model, :temperature, :additional_options
 
-      def initialize(model: DEFAULT_MODEL, temperature: DEFAULT_TEMPERATURE)
+      def initialize(model: DEFAULT_MODEL, temperature: DEFAULT_TEMPERATURE, **additional_options)
         @model = model
         @temperature = temperature
+        @additional_options = additional_options
       end
 
       def to_h
-        { model:, temperature: }
+        { model:, temperature:, **additional_options }.compact
       end
     end
   end
