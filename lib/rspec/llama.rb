42ebaff178da14298e7be19c4e8f40663592e0b2
White Rabbit <wwwwhiterabbit@proton.me>
2024-08-27 15:07:32 +0400

    refactor main module

--- lib/rspec/llama.rb
+++ lib/rspec/llama.rb
@@ -7,31 +7,35 @@ require_relative 'llama/helpers'
 
 module RSpec
   module Llama
-    # Model configurations
-    autoload :OpenaiModelConfiguration, 'rspec/llama/model_configurations/openai_model_configuration'
-    autoload :LlamaCppModelConfiguration, 'rspec/llama/model_configurations/llama_cpp_model_configuration'
-    autoload :OllamaModelConfiguration, 'rspec/llama/model_configurations/ollama_model_configuration'
+    {
+      # Model configurations
+      OpenaiModelConfiguration: 'rspec/llama/model_configurations/openai_model_configuration',
+      LlamaCppModelConfiguration: 'rspec/llama/model_configurations/llama_cpp_model_configuration',
+      OllamaModelConfiguration: 'rspec/llama/model_configurations/ollama_model_configuration',
 
-    # Model runners
-    autoload :OpenaiModelRunner, 'rspec/llama/model_runners/openai_model_runner'
-    autoload :LlamaCppModelRunner, 'rspec/llama/model_runners/llama_cpp_model_runner'
-    autoload :OllamaModelRunner, 'rspec/llama/model_runners/ollama_model_runner'
+      # Model runners
+      OpenaiModelRunner: 'rspec/llama/model_runners/openai_model_runner',
+      LlamaCppModelRunner: 'rspec/llama/model_runners/llama_cpp_model_runner',
+      OllamaModelRunner: 'rspec/llama/model_runners/ollama_model_runner',
 
-    # Model runner results
-    autoload :OpenaiModelRunnerResult, 'rspec/llama/model_runner_results/openai_model_runner_result'
-    autoload :LlamaCppModelRunnerResult, 'rspec/llama/model_runner_results/llama_cpp_model_runner_result'
-    autoload :OllamaModelRunnerResult, 'rspec/llama/model_runner_results/ollama_model_runner_result'
+      # Model runner results
+      OpenaiModelRunnerResult: 'rspec/llama/model_runner_results/openai_model_runner_result',
+      LlamaCppModelRunnerResult: 'rspec/llama/model_runner_results/llama_cpp_model_runner_result',
+      OllamaModelRunnerResult: 'rspec/llama/model_runner_results/ollama_model_runner_result',
 
-    # Model prompt
-    autoload :ModelPrompt, 'rspec/llama/model_prompt'
+      # Model prompt
+      ModelPrompt: 'rspec/llama/model_prompt',
 
-    # Model assertions
-    autoload :BaseModelAssertion, 'rspec/llama/model_assertions/base_model_assertion'
-    autoload :IncludeAllModelAssertion, 'rspec/llama/model_assertions/include_all_model_assertion'
-    autoload :IncludeAnyModelAssertion, 'rspec/llama/model_assertions/include_any_model_assertion'
-    autoload :ExcludeAllModelAssertion, 'rspec/llama/model_assertions/exclude_all_model_assertion'
+      # Model assertions
+      BaseModelAssertion: 'rspec/llama/model_assertions/base_model_assertion',
+      IncludeAllModelAssertion: 'rspec/llama/model_assertions/include_all_model_assertion',
+      IncludeAnyModelAssertion: 'rspec/llama/model_assertions/include_any_model_assertion',
+      ExcludeAllModelAssertion: 'rspec/llama/model_assertions/exclude_all_model_assertion',
 
-    # Model assertion result
-    autoload :ModelAssertionResult, 'rspec/llama/model_assertion_result'
+      # Model assertion result
+      ModelAssertionResult: 'rspec/llama/model_assertion_result'
+    }.each do |class_name, path|
+      autoload class_name, path
+    end
   end
 end
