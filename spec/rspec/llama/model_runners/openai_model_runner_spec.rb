23b084b7463a384b91414f3732c23f33f7372175
White Rabbit <10136844+wwwwhiterabbit@users.noreply.github.com>
2024-08-29 15:36:14 +0400

    the organization_id and project_id parameters are optional (#5)

--- spec/rspec/llama/model_runners/openai_model_runner_spec.rb
+++ spec/rspec/llama/model_runners/openai_model_runner_spec.rb
@@ -3,9 +3,9 @@
 RSpec.describe RSpec::Llama::OpenaiModelRunner do
   subject(:call_runner!) { described_class.new(**runner_options).call(model_configuration, model_prompt) }
 
-  let(:runner_options) { { access_token:, organization_id:, project_id: } }
-  let(:organization_id) { ENV.fetch('OPENAI_ORGANIZATION_ID') }
-  let(:project_id) { ENV.fetch('OPENAI_PROJECT_ID') }
+  let(:runner_options) { { access_token:, organization_id:, project_id: }.compact }
+  let(:organization_id) { nil }
+  let(:project_id) { nil }
 
   let(:model_configuration) do
     instance_double(
@@ -28,29 +28,57 @@ RSpec.describe RSpec::Llama::OpenaiModelRunner do
         to_s: 'Yes, Minsk is the capital of Belarus.'
       )
     end
-  end
 
-  context 'with invalid organization ID' do
-    let(:access_token) { ENV.fetch('OPENAI_ACCESS_TOKEN') }
-    let(:organization_id) { 'invalid_organization_id' }
+    context 'with valid organization ID' do
+      let(:organization_id) { ENV.fetch('OPENAI_ORGANIZATION_ID') }
 
-    it 'raises error', vcr: { cassette_name: 'openai_model_runner/invalid_organization_id' } do
-      expect { call_runner! }.to raise_error(
-        RSpec::Llama::OpenaiModelRunner::Error,
-        'No such organization: invalid_organization_id.'
-      )
+      it 'returns the response hash', vcr: {
+        cassette_name: 'openai_model_runner/valid_access_token_and_valid_organization_id'
+      } do
+        result = call_runner!
+
+        expect(result).to have_attributes(
+          class: RSpec::Llama::OpenaiModelRunnerResult,
+          to_s: 'Yes, Minsk is the capital of Belarus.'
+        )
+      end
+
+      context 'with valid project ID' do
+        let(:project_id) { ENV.fetch('OPENAI_PROJECT_ID') }
+
+        it 'returns the response hash', vcr: {
+          cassette_name: 'openai_model_runner/valid_access_token_and_valid_organization_id_and_valid_project_id'
+        } do
+          result = call_runner!
+
+          expect(result).to have_attributes(
+            class: RSpec::Llama::OpenaiModelRunnerResult,
+            to_s: 'Yes, Minsk is the capital of Belarus.'
+          )
+        end
+      end
+
+      context 'with invalid project ID' do
+        let(:project_id) { 'invalid_project_id' }
+
+        it 'raises error', vcr: { cassette_name: 'openai_model_runner/invalid_project_id' } do
+          expect { call_runner! }.to raise_error(
+            RSpec::Llama::OpenaiModelRunner::Error,
+            "The project '#{project_id}' does not exist in '#{organization_id}'"
+          )
+        end
+      end
     end
-  end
 
-  context 'with invalid project ID' do
-    let(:access_token) { ENV.fetch('OPENAI_ACCESS_TOKEN') }
-    let(:project_id) { 'invalid_project_id' }
+    context 'with invalid organization ID' do
+      let(:organization_id) { 'invalid_organization_id' }
 
-    it 'raises error', vcr: { cassette_name: 'openai_model_runner/invalid_project_id' } do
-      expect { call_runner! }.to raise_error(
-        RSpec::Llama::OpenaiModelRunner::Error,
-        "The project 'invalid_project_id' does not exist in '#{organization_id}'"
-      )
+      it 'raises error', vcr: { cassette_name: 'openai_model_runner/invalid_organization_id' } do
+        expect { call_runner! }.to raise_error(
+          RSpec::Llama::OpenaiModelRunner::Error,
+          "No such organization: #{organization_id}."
+        )
+      end
     end
   end
 
