8ebd5cec44a90ea0b690e6a4118a82af9e1eca75
Artur Antonnikau <arturantonnikau@gmail.com>
2024-10-01 19:23:04 +0200

    Update example spec

--- examples/spec/example_spec.rb
+++ examples/spec/example_spec.rb
@@ -11,6 +11,41 @@ RSpec.describe RSpec::Llama do
   let(:assertion) { build_model_assertion(:include_any, 'Yes') }
   let(:runner_result) { model_runner.call(model_configuration, prompt) }
 
+  shared_examples 'model assertion comparison' do |model, temperature = 0.5|
+    let(:model_runner) { build_model_runner(:openai, access_token: ENV.fetch('OPENAI_ACCESS_TOKEN')) }
+    let(:model_configuration) { build_model_configuration(:openai, model: model, temperature: temperature) }
+
+    it "validates assertion for model #{model} with temperature #{temperature}" do
+      expect(assertion_result).to be_passed
+    end
+  end
+
+  context 'comparison across models' do
+    models = { 'gpt-4o-mini' => 0.1, 'gpt-4o' => 0.5, 'gpt-4-turbo' => 0.1 }
+
+    models.each do |model_name|
+      include_examples 'model assertion comparison', model_name
+    end
+
+    context 'when adjusting prompt and assertion' do
+      let(:prompt) { build_model_prompt('What is the capital of Belarus?') }
+      let(:assertion) { build_model_assertion(:include_any, 'Minsk') }
+
+      models.each do |model_name, temperature|
+        include_examples 'model assertion comparison', model_name, temperature
+      end
+    end
+
+    context 'distance to the Moon' do
+      let(:prompt) { build_model_prompt('What is the distance to the Moon in kilometers?') }
+      let(:assertion) { build_model_assertion(:include_any, '384,400') }
+
+      models.each do |model_name, temperature|
+        include_examples 'model assertion comparison', model_name, temperature
+      end
+    end
+  end
+
   context 'with OpenAI model runner' do
     let(:model_runner) { build_model_runner(:openai, access_token: ENV.fetch('OPENAI_ACCESS_TOKEN')) }
     let(:model_configuration) { build_model_configuration(:openai, model:, temperature:) }
